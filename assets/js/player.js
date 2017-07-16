73dc024653095ebe03fab39b91fa35152404ea69
Bridger Herman <scooteeroo@gmail.com>
2017-07-16 13:08:55 -0500

    Clicks working for one measure

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,4 +1,5 @@
 let FREQS = [880.000000000000000, 1174.659071669630241, 1760.000000000000000];
+let DURATION = 30; // milliseconds
 let P = null;
 
 function printProps(obj) {
@@ -8,27 +9,33 @@ function printProps(obj) {
 }
 
 function init() {
-  tempo = document.getElementById('tempo').value;
-  P = new MeasurePlayer(tempo, 4, true);
+  var tempo = document.getElementById('tempo').value;
+  var beats = document.getElementById('beats').value;
+  var subdiv = document.getElementById('subdiv').value;
+  var accents = document.getElementById('db-accent').checked;
+  P = new MeasurePlayer(tempo, beats, subdiv, accents);
+  // T = new ToneGenerator(1);
 }
 
-function ToneGenerator() {
+function ToneGenerator(toneIndex) {
   this.audioContext = new (window.AudioContext || window.webkitAudioContext);
+  this.toneIndex = toneIndex;
   this.currentOsc = null;
   this.toneTimer = null;
-  this.toneDuration = 50; // milliseconds
 
   this.masterGainNode = this.audioContext.createGain();
   this.masterGainNode.connect(this.audioContext.destination);
   this.masterGainNode.gain.value = 0.5;
 
+  // TODO add performance.now() to have higher precision timing
   this.playTone = function(self) {
+    // console.log(self.toneIndex + " " + (Date.now()));
     self.currentOsc = self.audioContext.createOscillator();
     self.currentOsc.connect(self.masterGainNode);
-    self.currentOsc.frequency.value = FREQS[1];
+    self.currentOsc.frequency.value = FREQS[self.toneIndex];
     self.currentOsc.type = "sine";
     self.currentOsc.start();
-    self.toneTimer = setTimeout(self.stopTone, self.toneDuration, self); // TODO perhaps a better way than this...
+    self.toneTimer = setTimeout(self.stopTone, DURATION, self); // TODO perhaps a better way than this...
   }
 
   this.stopTone = function(self) {
@@ -39,23 +46,49 @@ function ToneGenerator() {
   }
 }
 
-function MeasurePlayer(tempo, beats, downbeatAccents) {
-  this.timer = null;
-  this.endTimer = null;
-  this.toneGenerator = new ToneGenerator();
-  this.timePerBeat = (60000)/tempo;
-  this.totalTime = this.timePerBeat * beats;
+function MeasurePlayer(tempo, beats, subdivs, downbeatAccents) {
+  this.beats = beats;
+  this.subdivs = subdivs;
+  this.numSubdivs = beats*subdivs;
 
+  this.timePerBeat = 60000/tempo;
+  this.timePerMeasure = this.timePerBeat*beats;
+  this.timePerSubdiv = this.timePerMeasure/this.numSubdivs;
 
-  this.play = function() {
-    this.timer = setInterval(this.toneGenerator.playTone, this.timePerBeat, this.toneGenerator);
-    this.endTimer = setTimeout(this.stop, this.totalTime, this);
+  this.dbTones = new ToneGenerator(2);
+  this.beatTones = new ToneGenerator(1);
+  this.subdivTones = new ToneGenerator(0);
+
+  this.timers = [];
+  for (var timerIndex = 0; timerIndex < this.numSubdivs; timerIndex++) {
+    this.timers[timerIndex] = null;
+  }
+
+  // TODO add a toggle feature
+  this.play = function(self) {
+    for (var timerIndex = 0; timerIndex < self.numSubdivs; timerIndex++) {
+      if (timerIndex % self.subdivs === 0) {
+        if (timerIndex === 0) {
+          // console.log("db");
+          this.timers[timerIndex] = setTimeout(self.dbTones.playTone, timerIndex*self.timePerSubdiv, self.dbTones);
+        }
+        else {
+          // console.log("beat");
+          this.timers[timerIndex] = setTimeout(self.beatTones.playTone, timerIndex*self.timePerSubdiv, self.beatTones);
+        }
+      }
+      else {
+        // console.log("sub");
+        this.timers[timerIndex] = setTimeout(self.subdivTones.playTone, timerIndex*self.timePerSubdiv, self.subdivTones);
+      }
+    }
   }
 
   this.stop = function(self) {
-    clearInterval(self.timer);
-    clearInterval(self.endTimer);
-    self.timer = null;
+    for (var timerIndex = 0; timerIndex < this.numSubdivs; timerIndex++) {
+      clearTimeout(this.timers[timerIndex]);
+      this.timers[timerIndex] = null;
+    }
   }
 }
 
