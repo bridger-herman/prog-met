1059b7c43a5416b93ab7ddf209054578f3450964
Bridger Herman <scooteeroo@gmail.com>
2017-07-17 12:05:41 -0500

    Split BasicPlayer into its own file

---
+++ assets/js/basicPlayer.js
@@ -0,0 +1,31 @@
+function BasicPlayer(measurePlayer) {
+  this.measurePlayer = measurePlayer;
+  this.timer = null;
+  this.playing = false;
+
+  this.play = function(self) {
+    self.measurePlayer.play(self.measurePlayer);
+    self.timer = setTimeout(self.play, self.measurePlayer.totalTime, self);
+  }
+
+  this.stop = function(self) {
+    self.measurePlayer.stop(self.measurePlayer);
+    clearTimeout(self.timer);
+  }
+
+  this.togglePlay = function() {
+    var objsToUpdate = $('.update-object');
+    this.playing = !this.playing;
+    if (this.playing === true) {
+      this.play(this);
+      $('#play-stop').html('pause');
+    }
+    else {
+      this.stop(this);
+      $('#play-stop').html('play_arrow');
+    }
+    for (var objIndex = 0; objIndex < objsToUpdate.length; objIndex++) {
+      objsToUpdate[objIndex].disabled = this.playing;
+    }
+  }
+}
